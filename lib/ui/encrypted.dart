b184d9e8f6bd1d2550903358cf4bd48b4ee0828d
glavotaner <marin.glavas@outlook.com>
2022-06-25 19:43:08 +0200

    fix encrypted tap

--- lib/ui/encrypted.dart
+++ lib/ui/encrypted.dart
@@ -7,34 +7,30 @@ class EncryptedText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: context.read<CipherCubit>().copyEncrypted,
-      child: BlocSelector<CipherCubit, CipherState, String>(
-        selector: (state) {
-          return (state as CipherReady).encrypted;
-        },
-        builder: (context, encrypted) {
-          if (encrypted.isNotEmpty) {
-            return Card(
-              child: InkWell(
-                onTap: () => 0,
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text(
-                    encrypted,
-                    style: const TextStyle(fontSize: 24),
-                  ),
+    return BlocSelector<CipherCubit, CipherState, String>(
+      selector: (state) {
+        return (state as CipherReady).encrypted;
+      },
+      builder: (context, encrypted) {
+        if (encrypted.isNotEmpty) {
+          return Card(
+            child: InkWell(
+              onTap: context.read<CipherCubit>().copyEncrypted,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  encrypted,
+                  style: const TextStyle(fontSize: 24),
                 ),
               ),
-            );
-          } else {
-            return const Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Text('Please put in somefin...'),
-            );
-          }
-        },
-      ),
+            ),
+          );
+        }
+        return const Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Text('Please put in somefin...'),
+        );
+      },
     );
   }
 }
